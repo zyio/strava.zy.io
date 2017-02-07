1461973c438a18a6b374955847eab1e8ab14f1a2
Sam Norbury <sam@zy.io>
2017-02-07 12:23:18 +0000

    Now with oauth functionality

--- main.rb
+++ main.rb
@@ -1,16 +1,38 @@
 require 'sinatra'
 require 'strava/api/v3'
 require 'json'
+require 'rest-client'
 
+load './env.rb' if File.exists?('./env.rb')
+
+set :environment, :production
 set :port, 4568
+use Rack::Session::Cookie, :key => 'rack.session', :path => '/', :secret => "#{ENV['cookie_secret']}"
 
 get '/' do
   @title = "Strava thing"
   erb :index
 end
 
+get '/sign_in' do
+  redirect "https://www.strava.com/oauth/authorize?client_id=#{ENV['oauth_client_id']}&response_type=code&redirect_uri=http://strava.zy.io/callback&approval_prompt=auto"
+end
+
+get '/sign_out' do
+  session[:access_token] = nil
+  redirect '/'
+end
+
+get '/callback' do
+  code = params[:code]
+  result = RestClient.post "https://www.strava.com/oauth/token", {client_id: "#{ENV['oauth_client_id']}", client_secret: "#{ENV['oauth_client_secret']}", code: "#{code}" }
+  jresult = JSON.parse(result)
+  session[:access_token] = jresult["access_token"]
+  redirect '/'
+end
+
 post '/results' do
-  @access_token = params['access_token']
+  @access_token = session[:access_token]
   @client = Strava::Api::V3::Client.new(:access_token => @access_token)
 
   @start_date = Date.parse(params['daterange'].split(' - ').first).to_time.to_i
@@ -42,3 +64,4 @@ post '/results' do
 
   erb :results
 end
+
